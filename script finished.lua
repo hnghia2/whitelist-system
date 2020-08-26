de67049dc48e5a363c94800d64580ee20ad7a468
hnghia2 <70258210+hnghia2@users.noreply.github.com>
2020-08-26 18:31:37 +0700

    Create script finished.lua

---
+++ script finished.lua
@@ -0,0 +1,7 @@
+local key = _G.Key
+local check = "https://raw.githubusercontent.com/hnghia2/whitelist-system/master/check.php?key=" .. key
+if game:HttpGet(check) == "Whitelisted" then
+loadstring(game:HttpGet("https://pastebin.com/raw/QCEmTvS0"))()
+else
+game.Players.LocalPlayer:Kick("Invalid Key! Please Rejoin And Try Again.")
+end
